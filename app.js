bcdf3a98fa6f72807247736510a629a69e99da41
biliomax <31713292+biliomax@users.noreply.github.com>
2018-09-13 02:12:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var app         = require('connect')()
-var serveStatic = require('serve-static')
+'ser strict'
 
-app.use(serveStatic('public'))
+const http = require('http');
+const debug = require('debug')('nodertr:server');
+//const xpto = require('./xpto/tese');
+const express = require('express');
 
-console.log(' ➜   Open: http://localhost:7007')
-app.listen(7007)
+const app = express();
+const  port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Route();
+
+const route = router.get('/',(req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+server.listen(port);
